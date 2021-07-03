4a0be42d9317d29545be09d7d1cf20defb2444f0
GitHub Action <action@github.com>
2021-07-03 11:13:04 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/sberthier/tsasyncconverter/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.sberthier.tsasyncconverter.services
+
+import com.github.sberthier.tsasyncconverter.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
